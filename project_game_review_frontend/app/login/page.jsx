34b9315b94839c5e47d694678977a05b07ae1c99
SamUnderscoreAsd <142822519+SamUnderscoreAsd@users.noreply.github.com>
2025-05-23 18:47:22 -0400

    added basic login page and onsubmit functionality. TODO: connect to backend and allow existing users to login

---
+++ project_game_review_frontend/app/login/page.jsx
@@ -0,0 +1,50 @@
+'use client'
+import React from 'react'
+import { useState } from 'react'
+
+const LoginPage = () => {
+const [username, setUsername] = useState('');
+const [password, setPassword] = useState('');
+
+const handleSubmit = (e) => {
+
+    e.preventDefault()
+
+    console.log(username);
+    console.log(password);
+
+}
+
+  return (
+    <div className = "p-20 h-full w-full">
+        <div className = "bg-white m-auto p-6 h-[50rem] w-2/5 rounded-md">
+            <div className = "text-black m-auto text-center ">
+                Login
+                <form onSubmit={handleSubmit}>
+                <div className = "p-6">
+                    <input 
+                    id="userName" 
+                    type="username" 
+                    placeholder='Username' 
+                    onChange = {(e) => setUsername(e.target.value)}
+                    className="px-3 bg-gray-700 text-white rounded-md"></input>
+                </div>
+                <div className = "p-6">
+                    <input 
+                    id="password" 
+                    type = "password" 
+                    placeholder='Password' 
+                    onChange = {(e) => setPassword(e.target.value)}
+                    className="px-3 bg-gray-700 text-white rounded-md"></input>
+                </div>
+                <div>
+                    <button type="submit" className = "bg-blue-500 px-3 text-center rounded-md">Submit</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ project_game_review_frontend/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import navBar from "../components/navBar"
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello Deez fucking nuts</h1>
+      <navBar></navBar>
+    </main>
+  );
+}
