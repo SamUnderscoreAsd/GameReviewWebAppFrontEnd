467e1114f3dba64ced8feab2938c62c1e0277e73
SamUnderscoreAsd <142822519+SamUnderscoreAsd@users.noreply.github.com>
2025-10-21 16:50:34 -0400

    create the basic component for the carosel. and began the initial styling

---
+++ components/gameCard.jsx
@@ -0,0 +1,9 @@
+import { React } from 'react'
+
+export default function GameCard(){
+    return (
+        <div className="bg-slate-400 w-[12.5rem] h-[20rem] rounded-md">
+            <img className="" href='https://lazoo.org/wp-content/uploads/2023/05/Capuchin-JEP_5945-1024x731.jpg'/>
+        </div>
+    )
+}
