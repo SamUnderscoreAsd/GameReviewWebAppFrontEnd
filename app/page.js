139824ff8e1eb67fb7c86bee90d7ac307e40f839
SamUnderscoreAsd <142822519+SamUnderscoreAsd@users.noreply.github.com>
2025-10-28 13:13:47 -0400

    began work on creating a game card page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
+import GameCard from "@/components/gameCard";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="h-screen w-screen flex flex-col">
       <div className = "my-10 w-full h-5/6 bg-white text-black flex justify-center items-center text-center">
-          <div>
-            Carosel
-          </div>
+          <GameCard>{'Test'}</GameCard>
       </div>
       <div className = "my-10 w-full h-2/6 bg-white text-black">
         Category 1
--- components/gameCard.jsx
+++ components/gameCard.jsx
@@ -1,9 +1,13 @@
 import { React } from 'react'
 
-export default function GameCard(){
+export default function GameCard(gameName){
+    
     return (
-        <div className="bg-slate-400 w-[12.5rem] h-[20rem] rounded-md">
-            <img className="" href='https://lazoo.org/wp-content/uploads/2023/05/Capuchin-JEP_5945-1024x731.jpg'/>
+        <div className="bg-slate-400 w-[12.5rem] h-[20rem] rounded-md items-center content-center shadow-black shadow-md overflow-hidden relative">
+            <img className="w-full h-full object-cover " src='https://lazoo.org/wp-content/uploads/2023/05/Capuchin-JEP_5945-1024x731.jpg'/>
+            <div className='absolute top-0 left-0 right-0 items-center content-start'>
+                <h3 className="text-white">{gameName}</h3>
+            </div>
         </div>
     )
 }
