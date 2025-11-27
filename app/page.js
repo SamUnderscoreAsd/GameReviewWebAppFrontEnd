7749a14d0611a14b7bb4a64c772051adb9464a45
SamUnderscoreAsd <fssamfarajzadeh@gmail.com>
2025-11-26 19:56:34 -0500

    big change incoming: Added carousel component, added gameCard component, fixed the layout being so ugly, removed the unecessary scrollbars

--- app/page.js
+++ app/page.js
@@ -1,17 +1,87 @@
 import GameCard from "@/components/gameCard";
+import HorizontalCarosel from "@/components/horizontalCarosel";
 import Image from "next/image";
 
 export default function Home() {
+
+  const fakeGames = [
+  {
+    title: "Mystic Warriors",
+    imageUrl: "https://picsum.photos/seed/game1/400/600",
+    rating: 8.2
+  },
+  {
+    title: "Dragon's Fury",
+    imageUrl: "https://picsum.photos/seed/game2/400/600",
+    rating: 9
+  },
+  {
+    title: "Cyber Legends",
+    imageUrl: "https://picsum.photos/seed/game3/400/600",
+    rating: 7
+  },
+  {
+    title: "Shadow Realm",
+    imageUrl: "https://picsum.photos/seed/game4/400/600",
+    rating: 10
+  },
+  {
+    title: "Pixel Quest",
+    imageUrl: "https://picsum.photos/seed/game5/400/600",
+    rating: 6
+  },
+  {
+    title: "Neon Strike",
+    imageUrl: "https://picsum.photos/seed/game6/400/600",
+    rating: 8
+  },
+  {
+    title: "Ancient Prophecy",
+    imageUrl: "https://picsum.photos/seed/game7/400/600",
+    rating: 9
+  },
+  {
+    title: "Star Voyager",
+    imageUrl: "https://picsum.photos/seed/game8/400/600",
+    rating: 7
+  },
+  {
+    title: "Shadow Realm",
+    imageUrl: "https://picsum.photos/seed/game4/400/600",
+    rating: 10
+  },
+  {
+    title: "Pixel Quest",
+    imageUrl: "https://picsum.photos/seed/game5/400/600",
+    rating: 6
+  },
+  {
+    title: "Neon Strike",
+    imageUrl: "https://picsum.photos/seed/game6/400/600",
+    rating: 8
+  },
+  {
+    title: "Ancient Prophecy",
+    imageUrl: "https://picsum.photos/seed/game7/400/600",
+    rating: 9
+  },
+  {
+    title: "Star Voyager",
+    imageUrl: "https://picsum.photos/seed/game8/400/600",
+    rating: 7
+  }
+];
+
   return (
-    <div className="h-screen w-screen flex flex-col">
+    <div className="h-auto w-screen flex flex-col">
       <div className = "my-10 w-full h-5/6 bg-white text-black flex justify-center items-center text-center">
-          <GameCard>{'Test'}</GameCard>
+          <HorizontalCarosel gameList={fakeGames}></HorizontalCarosel>
       </div>
-      <div className = "my-10 w-full h-2/6 bg-white text-black">
-        Category 1
+      <div className = "my-10 w-full h-5/6 bg-white text-black flex justify-center items-center text-center">
+          <HorizontalCarosel gameList={fakeGames}></HorizontalCarosel>
       </div>
-      <div className = "my-10 w-full h-2/6 bg-white text-black">
-        Category 2
+      <div className = "my-10 w-full h-5/6 bg-white text-black flex justify-center items-center text-center">
+          <HorizontalCarosel gameList={fakeGames}></HorizontalCarosel>
       </div>
     </div>
   );
--- components/gameCard.jsx
+++ components/gameCard.jsx
@@ -1,12 +1,15 @@
 import { React } from 'react'
 
-export default function GameCard(gameName){
+export default function GameCard({gameName, rating, thumbnail}){
     
     return (
-        <div className="bg-slate-400 w-[12.5rem] h-[20rem] rounded-md items-center content-center shadow-black shadow-md overflow-hidden relative">
-            <img className="w-full h-full object-cover " src='https://lazoo.org/wp-content/uploads/2023/05/Capuchin-JEP_5945-1024x731.jpg'/>
-            <div className='absolute top-0 left-0 right-0 items-center content-start'>
-                <h3 className="text-white">{gameName}</h3>
+        <div className="bg-slate-400 m-5 w-[12.5rem] h-[20rem] rounded-md items-center content-center shadow-black shadow-lg overflow-hidden relative hover:scale-105 hover:transition-transform hover:duration-300">
+            <img className="select-none pointer-events-none w-full h-full object-cover " src={thumbnail}/>
+            <div className='select-none absolute top-0 left-0 right-0 items-center content-start'>
+                <p className='text-white text-2xl'>{gameName}</p>
+            </div>
+            <div className="relative bottom-12 select-none">
+                <p className="text-white text-5xl">{rating}/10</p>
             </div>
         </div>
     )
